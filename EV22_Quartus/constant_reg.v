e11a6cc131487c29f17de4e36b936e8e65377e75
fmoriconi <31166325+fmoriconi@users.noreply.github.com>
2022-06-08 06:42:38 -0300

    Super Register Bank
    Hay que probar todo de vuelta porque cambié todo para poder hacer un fix que no se podía con el register bank original. El register bank nuevo respeta el modelo propuesto por la consigna.

--- EV22_Quartus/constant_reg.v
+++ EV22_Quartus/constant_reg.v
@@ -5,7 +5,7 @@ module constant_reg ( input [7:0] k_in,
 							 output reg [15:0] k_out);
 						 
 	//always @ (clk) begin
-	always @ (negedge clk) begin
+	always @ (posedge clk) begin
 		if(ena) begin
 			if(byte_select)
 				k_out = {k_in, k_out[7:0]};
--- EV22_Quartus/super_register_bank.v
+++ EV22_Quartus/super_register_bank.v
@@ -5,6 +5,7 @@ module super_register_bank(
 									input[15:0] Data_C,
 									input[5:0] Sel_C,
 									input clk,
+									input nreset,
 									input[4:0] Sel_A,
 									input[5:0] Sel_B,
 									input[15:0] PI0,
@@ -14,58 +15,79 @@ module super_register_bank(
 									output reg[15:0] Data_B,
 									output reg[15:0] Working_register,
 									output reg[15:0] PO0,
-									output reg[15:0] PO1
+									output reg[15:0] PO1,
+									output[15:0] DATA,
+									output[5:0] SEL_REG,
+									output[15:0] DATA_A_net,
+									output[15:0] Data_B_net
 									);
 									
-	wire DATA;
-	wire SEL_REG;
+	wire updateFlag;
+//	wire SEL_REG;
+	wire W_MEM_OUT_net;
+
+//	wire Data_A_net;
+//	wire Data_B_net;
+	
+	wire reset;
+	assign reset = ~nreset;
 				
 	reg[15:0] r0; reg[15:0] r1; reg[15:0] r2; reg[15:0] r3; reg[15:0] r4; reg[15:0] r5; reg[15:0] r6; reg[15:0] r7;
 	reg[15:0] r8; reg[15:0] r9; reg[15:0] r10; reg[15:0] r11; reg[15:0] r12; reg[15:0] r13; reg[15:0] r14; reg[15:0] r15;
 	reg[15:0] r16; reg[15:0] r17; reg[15:0] r18; reg[15:0] r19; reg[15:0] r20; reg[15:0] r21; reg[15:0] r22; reg[15:0] r23;
-	reg[15:0] r24; reg[15:0] r25; reg[15:0] r26; reg[15:0] r27;  reg[15:0] r28;  reg[15:0] r29;  reg[15:0] r30;  reg[15:0] r31;
+	reg[15:0] r24; reg[15:0] r25; reg[15:0] r26; reg[15:0] r27;  reg[15:0] r28;  reg[15:0] r29; //Los outputs son declarados en las salidas del modulo
 	reg[15:0] r32;  reg[15:0] r33;
 
 				
-	block2 block2(.clk(clk), .W_IN(Working_register), .W_MEM_IN(W_MEM_IN), .MR(MR), .MW(MW), .Data_C(DATA_C), .Sel_C(SEL_C), .W_MEM_OUT(W_MEM_OUT), .DATA(DATA), .SEL_REG(SEL_REG));
-	block3 block3(.Sel_A(SEL_A), .Sel_B(SEL_B), .Working_Register(Working_Register), .r0(r0),  .r0(r0),  .r1(r1),  .r2(r2),  .r3(r3),  .r4(r4),  .r5(r5),  .r6(r6),  .r7(r7),  .r8(r8),  .r9(r9),  .r10(r10),  .r11(r11),  .r12(r12),  .r13(r13),   .r14(r14), .r15(r15), .r16(r16), .r17(r17), .r18(r18), .r19(r19), .r20(r20), .r21(r21), .r22(r22), .r23(r23), .r24(r24), .r25(r25), .r26(r26), .r27(r27), .r28(PI0),  .r29(PI1),  .r32(r32),  .r33(r33), .Data_A(Data_A), .Data_B(Data_B));
+	Block2 block2(.clk(clk), .W_IN(Working_register), .W_MEM_IN(W_MEM_IN), .MR(MR), .MW(MW), .Data_C(Data_C), .Sel_C(Sel_C), .W_MEM_OUT(W_MEM_OUT_net), .DATA(DATA), .SEL_REG(SEL_REG));
+	Block3 block3(.Sel_A(Sel_A), .Sel_B(Sel_B), .Working_Register(Working_register), .r0(r0), .r1(r1),  .r2(r2),  .r3(r3),  .r4(r4),  .r5(r5),  .r6(r6),  .r7(r7),  .r8(r8),  .r9(r9),  .r10(r10),  .r11(r11),  .r12(r12),  .r13(r13),   .r14(r14), .r15(r15), .r16(r16), .r17(r17), .r18(r18), .r19(r19), .r20(r20), .r21(r21), .r22(r22), .r23(r23), .r24(r24), .r25(r25), .r26(r26), .r27(r27), .r28(PI0),  .r29(PI1),  .r32(r32),  .r33(r33), .Data_A(Data_A_net), .Data_B(Data_B_net));
 
-	always @ (DATA, SEL_REG) begin
-		case(Sel_C)
-				0: 		r0 = Data_C;
-				1: 		r1 = Data_C;
-				2: 		r2 = Data_C;
-				3: 		r3 = Data_C;
-				4: 		r4 = Data_C;
-				5: 		r5 = Data_C;
-				6: 		r6 = Data_C;
-				7: 		r7 = Data_C;
-				8: 		r8 = Data_C;
-				9: 		r9 = Data_C;
-				10: 		r10 = Data_C;
-				11: 		r11 = Data_C;
-				12: 		r12 = Data_C;
-				13: 		r13 = Data_C;
-				14: 		r14 = Data_C;
-				15: 		r15 = Data_C;
-				16: 		r16 = Data_C;
-				17: 		r17 = Data_C;
-				18: 		r18 = Data_C;
-				19: 		r19 = Data_C;
-				20: 		r20 = Data_C;
-				21: 		r21 = Data_C;
-				22: 		r22 = Data_C;
-				23: 		r23 = Data_C;
-				24: 		r24 = Data_C;
-				25: 		r25 = Data_C;
-				26: 		r26 = Data_C;
-				27: 		r27 = Data_C;
+	always @ (Data_A_net, Data_B_net, SEL_REG, nreset) begin
+	
+	Data_A <= Data_A_net;
+	Data_B <= Data_B_net;
+	W_MEM_OUT <= W_MEM_OUT_net;
+	
+	if (reset) begin
+				r0  <= 0;  r1 <= 0;  r2 <= 0;  r3 <= 0;  r4 <= 0;  r5 <= 0;  r6 <= 0;  r7 <= 0;  r8 <= 0;  r9 <= 0;
+				r10 <= 0; r11 <= 0; r12 <= 0; r13 <= 0; r14 <= 0; r15 <= 0; r16 <= 0; r17 <= 0; r18 <= 0; r19 <= 0;
+				r20 <= 0; r21 <= 0; r22 <= 0; r23 <= 0; r24 <= 0; r25 <= 0; r26 <= 0; r27 <= 0;
+				PO0 <= 0; PO1 <= 0; Working_register <= 0;
+	end else begin
+		case(SEL_REG)
+				0: 		r0 <= DATA;
+				1: 		r1 <= DATA;
+				2: 		r2 <= DATA;
+				3: 		r3 <= DATA;
+				4: 		r4 <= DATA;
+				5: 		r5 <= DATA;
+				6: 		r6 <= DATA;
+				7: 		r7 <= DATA;
+				8: 		r8 <= DATA;
+				9: 		r9 <= DATA;
+				10: 		r10 <= DATA;
+				11: 		r11 <= DATA;
+				12: 		r12 <= DATA;
+				13: 		r13 <= DATA;
+				14: 		r14 <= DATA;
+				15: 		r15 <= DATA;
+				16: 		r16 <= DATA;
+				17: 		r17 <= DATA;
+				18: 		r18 <= DATA;
+				19: 		r19 <= DATA;
+				20: 		r20 <= DATA;
+				21: 		r21 <= DATA;
+				22: 		r22 <= DATA;
+				23: 		r23 <= DATA;
+				24: 		r24 <= DATA;
+				25: 		r25 <= DATA;
+				26: 		r26 <= DATA;
+				27: 		r27 <= DATA;
 				//28: No es posible escribir sobre un puerto de entrada
 				//29: No es posible escribir sobre un puerto de entrada
-				30: 		r30 = Data_C; //Output 0
-				31: 		r31 = Data_C; //Output 1
-				
-				34: 		Working_register = Data_C; //W_DATA <= Data_C; end
+				30: 		PO0 <= DATA; //Output 0
+				31: 		PO1 <= DATA; //Output 1
+				34: 		Working_register <= DATA; //W_DATA <= Data_C; end
 				
 				//default: begin Working_Reg = Data_C;  W_OUT = Data_C; end
 				//Lo comenté por el siguiente bug:
@@ -74,9 +96,7 @@ module super_register_bank(
 				// Luego, como C = 0 y el código C_BUS del opcode es 35, por defecto el register bank cargaba Working_Reg = DATA_C = 0. Entonces, no lograba hacer saltar al código porque W ya no valía K, si no 0.
 				//Comenté el default y anduvo.
 			endcase
-	
-	
-	
+		end
 	end
 									
 endmodule
