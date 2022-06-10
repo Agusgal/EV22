3fcee693a26ba581be8bb02b8aac2c71b3a80146
fmoriconi <31166325+fmoriconi@users.noreply.github.com>
2022-06-10 00:32:48 -0300

    Bug "fix" Carry Out

--- EV22_Quartus/alu.v
+++ EV22_Quartus/alu.v
@@ -8,20 +8,20 @@ module ALU ( input [15:0] b,
 	
 	always @ (b, a, aluc, cy_in) begin
 		case(aluc)
-			4'b0000 : z = a;
-			4'b0001 : z = b;
-			4'b0010 : z = ~a;
-			4'b0011 : z = ~b;
-			4'b0100 : {cy_out, z} = a + b;
-			4'b0101 : {cy_out, z} = a + b + cy_in;
-			4'b0110 : z = a | b;
-			4'b0111 : z = a & b;
-			4'b1000 : z = 0;
-			4'b1001 : z = 1;
-			4'b1010 : z = 16'hFFFF;
-			4'b1011 : cy_out = 0;
-			4'b1100 : cy_out = 1;
-			default : {cy_out, z} = 0;
+			4'b0000 : z <= a;
+			4'b0001 : z <= b;
+			4'b0010 : z <= ~a;
+			4'b0011 : z <= ~b;
+			4'b0100 : {cy_out, z} <= a + b; 
+			4'b0101 : {cy_out, z} <= a + b + cy_in;
+			4'b0110 : z <= a | b;
+			4'b0111 : z <= a & b; 
+			4'b1000 : z <= 0; 
+			4'b1001 : z <= 1; 
+			4'b1010 : z <= 16'hFFFF;
+			4'b1011 : cy_out <= 0;
+			4'b1100 : cy_out <= 1;
+	//default : {cy_out, z} = 0;
 
 		endcase
 	end		
--- EV22_Quartus/carry_block.v
+++ EV22_Quartus/carry_block.v
@@ -3,12 +3,12 @@ module carry_block ( input cy_out,
 							input [3:0] aluc, 
 							output reg cy);
 	//Creo que esto lo que hace es preparar el carry para la próxima operación
-	always @ (posedge clk) begin
+	always @ (posedge clk /*cy_out, aluc*/) begin
 		case(aluc)
-			4'b0100: cy <= cy_out; //A+B
-			4'b0101: cy <= cy_out; //A+B+CY
-			4'b1011: cy <= cy_out; //CY=0
-			4'b1100: cy <= cy_out; //CY=1
+			4'b0100: cy = cy_out; //A+B
+			4'b0101: cy = cy_out; //A+B+CY
+			4'b1011: cy = cy_out; //CY=0
+			4'b1100: cy = cy_out; //CY=1
 		endcase
 	end
 endmodule
