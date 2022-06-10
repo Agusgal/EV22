cee23d4be5f2dd2e449715d337c85307ec504b56
fmoriconi <31166325+fmoriconi@users.noreply.github.com>
2022-06-09 21:20:17 -0300

    Previo a repaso final de instrucciones

--- EV22_Quartus/constant_reg.v
+++ EV22_Quartus/constant_reg.v
@@ -2,11 +2,19 @@ module constant_reg ( input [7:0] k_in,
 							 input byte_select,
 							 input ena,
 							 input clk,
+							 input nreset,
 							 output reg [15:0] k_out);
 						 
+						 
+		
+	wire reset;
+	assign reset = ~nreset;
+						 
 	//always @ (clk) begin
-	always @ (posedge clk) begin
-		if(ena) begin
+	always @ (posedge clk, posedge reset) begin
+		if(reset) begin
+			k_out = 0;
+		end else if(ena) begin
 			if(byte_select)
 				k_out = {k_in, k_out[7:0]};
 			else
