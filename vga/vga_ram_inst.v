825969c44f52fa5ec519fa34cc9fb9e125b04be9
fscapolla <42211120+fscapolla@users.noreply.github.com>
2022-06-26 12:50:04 -0300

    Added VGA controller as a separate project

---
+++ vga/vga_ram_inst.v
@@ -0,0 +1,10 @@
+vga_ram	vga_ram_inst (
+	.data ( data_sig ),
+	.rdaddress ( rdaddress_sig ),
+	.rdclock ( rdclock_sig ),
+	.rden ( rden_sig ),
+	.wraddress ( wraddress_sig ),
+	.wrclock ( wrclock_sig ),
+	.wren ( wren_sig ),
+	.q ( q_sig )
+	);
